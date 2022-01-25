ae99fe51d907133c6c05ec3c7825bcb42d0c34b5
roleplayeer <98228802+roleplayeer@users.noreply.github.com>
2022-01-26 03:46:56 +0700

    Create rahasia.py

---
+++ rahasia.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import random
+from datetime import datetime
+from os import execl
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
+from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 , STRING26 , STRING27 , STRING28 , STRING29 , STRING30
+import asyncio
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from Utils import RAID, EAGLE , HPRAID , RRAID
+
+
+a = API_ID
+b = API_HASH
+rommel = STRING
+ojie = STRING2
+tata = STRING3
+siska = STRING4
+koko = STRING5
